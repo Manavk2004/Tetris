692778eb9958728fc1adc22f12635e815198b774
Manavk2004 <mjkamdar04@gmail.com>
2025-04-18 22:09:44 -0400

    Check for completed rows

--- game.py
+++ game.py
@@ -22,12 +22,12 @@ class Game:
 
     def move_left(self):
         self.current_block.move(0, -1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move(0,1)
 
     def move_right(self):
         self.current_block.move(0,1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move(0, -1)
 
 
@@ -43,6 +43,7 @@ class Game:
             self.grid.grid[position.row][position.column] = self.current_block.id
         self.current_block = self.next_block
         self.next_block = self.get_random_block()
+        self.grid.clear_full_rows()
 
 
     def block_fits(self):
@@ -55,7 +56,7 @@ class Game:
 
     def rotate(self):
         self.current_block.rotate()
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.undo_rotation()
 
     def block_inside(self):
